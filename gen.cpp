b27bc8d9fed73e501b94fc3b1dba7d9b6eea27fb
Anthony Wang <ta180m@gmail.com>
2020-06-08 13:55:54 -0500

    Create gen.cpp

---
+++ gen.cpp
@@ -0,0 +1,16 @@
+/*
+   ______         
+  / ____/__    __ 
+ / /  __/ /___/ /_
+/ /__/_  __/_  __/
+\____//_/   /_/   
+    125 characters
+
+Usage:
+g++ gen.cpp -o passgen
+passgen [KEY] [SITE]
+*/
+
+#include<iostream>
+#define o(x) std::cout<<x
+main(int argc,char*argv[]){o(argv[1]);for(int i=0;i<4;++i)o(char(argv[2][i]^37));}
