804c3e073621d295eca96acd185cc3b516f11590
Anthony Wang <ta180m@gmail.com>
2020-06-08 14:54:11 -0500

    Update gen.cpp

--- gen.cpp
+++ gen.cpp
@@ -4,7 +4,7 @@
  / /  __/ /___/ /_
 / /__/_  __/_  __/
 \____//_/   /_/   
-    125 characters
+    124 characters
 
 Usage:
 g++ gen.cpp -o gen
@@ -13,4 +13,4 @@ gen [KEY] [SITE]
 
 #include<iostream>
 #define o(x) std::cout<<x
-main(int argc,char*argv[]){o(argv[1]);for(int i=0;i<4;++i)o(char(argv[2][i]^37));}
+main(int argc,char**argv){o(argv[1]);for(int i=0;i<4;++i)o(char(argv[2][i]^37));}
