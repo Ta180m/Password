5ff4b8f1e4f4c084fcf5b90ba0655de2454fdde1
Anthony Wang <ta180m@gmail.com>
2020-06-08 15:54:23 -0500

    Update gen2.cpp

--- gen2.cpp
+++ gen2.cpp
@@ -16,4 +16,4 @@ char f(int x){return x*(x*(29-2
 *x)-91)/2+97;}main(int d,char**
 v){std::cout<<v[1];for(int i=0;
 i<4;++i){char x=(v[2][i]-97)%16
-;std::cout<<f(x/4)<<f(x%4);}}
+;std::cout<<f(x/4)<<f(x%4);}}//
