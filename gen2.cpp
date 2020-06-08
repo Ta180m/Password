ee2ab393ae8725a1eea76e947293dbc45ba2ef14
Anthony Wang <ta180m@gmail.com>
2020-06-08 15:36:05 -0500

    Update gen2.cpp

--- gen2.cpp
+++ gen2.cpp
@@ -4,7 +4,7 @@
  / /  __/ /___/ /_
 / /__/_  __/_  __/
 \____//_/   /_/   
-    214 characters
+    171 characters
 
 Usage:
 g++ gen2.cpp -o gen2
@@ -12,13 +12,8 @@ gen2 [KEY] [SITE]
 */
 
 #include<iostream>
-#define c case
-#define r return
-char f(int x){switch(x)
-{c 0:r'a';c 1:r'A';c 2:
-r'8';}r'@';}main(int d,
-char**v){std::cout<<v[1
-];for(int i=0;i<4;++i){
-char x=(v[2][i]-97)%16;
-std::cout<<f(x/4)<<f(x%
-4);}}
+char f(int x){return x*(x*(29-2
+*x)-91)/2+97;}main(int d,char**
+v){std::cout<<v[1];for(int i=0;
+i<4;++i){char x=(v[2][i]-97)%16
+;std::cout<<f(x/4)<<f(x%4);}}
