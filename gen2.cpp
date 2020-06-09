df59fea102249693a3a69cf12627de011cee4aa5
Anthony Wang <ta180m@gmail.com>
2020-06-08 21:01:04 -0500

    Update gen2.cpp

--- gen2.cpp
+++ gen2.cpp
@@ -4,13 +4,16 @@
  / /  __/ /___/ /_
 / /__/_  __/_  __/
 \____//_/   /_/   
-    160 characters
+    146 characters
 
 Usage:
 g++ gen2.cpp -o gen2
 gen2 [KEY] [SITE]
 */
 
-#include<iostream>
-int f(int x){std::cout<<char(((29-2*x)*x-91)*x/2+97);}main(int d,char**
-v){std::cout<<v[1];for(int i=0;i<4;++i)f(--v[2][i]%16/4),f(v[2][i]%4);}
+#include<cstdio>
+int i;int f(int x){putchar
+(((29-2*x)*x-91)*x/2+97);}
+main(int c,char**v){printf
+(v[1]);for(;i<4;++i)f(--v[
+2][i]%16/4),f(v[2][i]%4);}
