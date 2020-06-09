25d6166c8eab9f91bf9c72f57cb26ab356b9a57c
Anthony Wang <ta180m@gmail.com>
2020-06-08 20:17:37 -0500

    Update gen2.cpp

--- gen2.cpp
+++ gen2.cpp
@@ -12,4 +12,5 @@ gen2 [KEY] [SITE]
 */
 
 #include<iostream>
-int f(int x){std::cout<<char(((29-2*x)*x-91)*x/2+97);}main(int d,char**v){std::cout<<v[1];for(int i=0;i<4;++i)f(--v[2][i]%16/4),f(v[2][i]%4);}
+int f(int x){std::cout<<char(((29-2*x)*x-91)*x/2+97);}main(int d,char**
+v){std::cout<<v[1];for(int i=0;i<4;++i)f(--v[2][i]%16/4),f(v[2][i]%4);}
